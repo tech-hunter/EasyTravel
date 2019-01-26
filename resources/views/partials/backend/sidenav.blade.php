4ef2c1d9de8c4ebaab33af8462d14e8b79fb4074
NIKHIL-PC\nikhi <nikhil.lu210@gmail.com>
2019-01-27 03:23:35 +0600

    Admin Allpage Controller

--- resources/views/partials/backend/sidenav.blade.php
+++ resources/views/partials/backend/sidenav.blade.php
@@ -35,18 +35,40 @@
                 </ul>
             </li>
 
-            <li class="nav-title">More</li>
-
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="fas fa-route"></i> Tour
+            <li class="nav-item nav-dropdown">
+                <a href="#" class="nav-link nav-dropdown-toggle">
+                    <i class="fas fa-box-open"></i> Offers <i class="fa fa-caret-left"></i>
                 </a>
+
+                <ul class="nav-dropdown-items">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="fas fa-truck-pickup"></i> Tour
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="fas fa-hospital"></i> Treatment
+                        </a>
+                    </li>
+                </ul>
             </li>
 
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="far fa-hospital"></i> Treatment
+            <li class="nav-title">More</li>
+
+            <li class="nav-item nav-dropdown">
+                <a href="#" class="nav-link nav-dropdown-toggle">
+                    <i class="fas fa-tools"></i> Settings <i class="fa fa-caret-left"></i>
                 </a>
+
+                <ul class="nav-dropdown-items">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="fas fa-map"></i> Control States
+                        </a>
+                    </li>
+                </ul>
             </li>
         </ul>
     </nav>
