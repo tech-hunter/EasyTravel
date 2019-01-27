e401cac48e6fa945134d112594c5ca9a6ca8dd4d
proma <modhupromz01@gmail.com>
2019-01-28 02:21:30 +0600

    generate state part table add

--- resources/views/admin/states/index.blade.php
+++ resources/views/admin/states/index.blade.php
@@ -13,7 +13,7 @@
         <div class="col-md-8 offset-2">
             <div class="card">
                 <div class="card-header">
-                    generate states
+                    Generate States
                 </div>
                 <div class="card-body">
                     <form action="{{ route('admin.state.store') }}" method="POST">
@@ -28,17 +28,58 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-8 offset-2">
+</div>
 
-            <ul class="list-group">
-                @foreach ($states as $key => $state)
-                <li class="list-group-item disabled">{{ $key+1 }} name is {{ $state->name }}
-                    <a href="{{ route('admin.state.edit', ['id' => $state->id]) }}">edit</a>
-                    <a href="{{ route('admin.state.destroy', ['id' => $state->id]) }}">delete</a>
-                </li>
-                @endforeach
-            </ul>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header bg-light">
+                    <strong>Generated States</strong>
+    
+                    <div class="card-actions">
+                        {{-- <a href="#" class="btn">
+                            <i class="fa fa-pencil-alt"></i>
+                        </a> --}}
+    
+                        <a href="{{Route('admin.air.create')}}" class="btn">
+                            <i class="fa fa-plus"></i>
+                        </a>
+                    </div>
+                </div>
+    
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>State Name</th>
+                                    <th class="action">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($states as $key => $state)
+                                    <tr>
+                                        <td>{{ $key+1 }}</td>
+                                        <td>{{ $state->name }}</td>
+                                        <td class="action">
+                                            <a href="{{ Route( 'admin.state.edit',['id'=>$state->id] ) }}" class="btn">
+                                                <i class="fa fa-pencil-alt"></i>
+                                            </a>
+                        
+                                            <a href="{{ Route( 'admin.state.destroy',['id'=>$state->id] ) }}" onclick="return confirm('Delete! Are you sure?')" class="btn">
+                                                <i class="fa fa-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
